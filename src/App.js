e15ef447c305c94ea93e91b365a5d66b063d71f7
AntrikshNarang <itsantriksh@gmail.com>
2023-03-18 18:38:42 +0530

    Added Routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Intro from './components/Intro';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login-Signup/Login';
+import Signup from './components/Login-Signup/Signup';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Navbar/>
         <Routes>
           <Route exact path='/' element={<Intro />} />
+          <Route exact path='/login' element={<Login/>} />
+          <Route exact path='/signup' element={<Signup/>} />
         </Routes>
       </Router>
     </>
