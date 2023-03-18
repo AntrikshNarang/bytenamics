a4fab9e64e92aa3c84673f0bab04c3a246aeeaba
AntrikshNarang <itsantriksh@gmail.com>
2023-03-18 18:40:32 +0530

    Fixed hyperlink Error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,13 +7,13 @@ const Navbar=()=> {
        <div className="header">
         <nav id="site-navigation" className="header-nav">
             <div className="branding">
-                <a className="hov" href="#">Bytenamics</a>
+                <a className="hov" href="/">Bytenamics</a>
             </div>
             <div className="mid">
                 <ul className="navigation">
-                    <li><a href="#" className="nav">Home</a> </li>
-                    <li><a href="#" className="nav">Events</a> </li>
-                    <li><a href="#" className="nav">About Us</a> </li>
+                    <li><a href="/" className="nav">Home</a> </li>
+                    <li><a href="/" className="nav">Events</a> </li>
+                    <li><a href="/" className="nav">About Us</a> </li>
                     <li><a href="contact.html" className="nav" target="_blank">Contact Us</a> </li>
                 </ul>
             </div>
